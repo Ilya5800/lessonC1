4b1aa32a4e282d4d663e3dec8efdf07796578da0
Ilya5800 <122695775+Ilya5800@users.noreply.github.com>
2023-01-29 16:15:05 +0300

    ..m

--- 015/Program.cs
+++ 015/Program.cs
@@ -5,13 +5,13 @@ int a = Convert.ToInt32(Console.ReadLine());
 
 //345
 //12345
-int b=a/100%10;
-if (b==0)
+
+if (a<=100)
 {
 System.Console.WriteLine("No");
 }
 else
 {
 if (b>0)
-System.Console.WriteLine(b);
+System.Console.WriteLine(a/100%10);
 }
