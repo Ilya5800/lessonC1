b4cd6d786f374532ff6ec8d1e60dc77e886a9257
Ilya5800 <ilyasemin870@gmail.com>
2023-02-09 04:01:19 +0300

    Home Work4

---
+++ 016_2/Program.cs
@@ -0,0 +1,4 @@
+﻿// Дано число. Проверить крастно ли оно 7 и 23
+int n=Convert.ToInt32(Console.ReadLine());
+if (n%7==0 && n%23==0) System.Console.WriteLine("YES"); 
+else System.Console.WriteLine("NO");
---
+++ 022/Program.cs
@@ -0,0 +1,15 @@
+﻿
+System.Console.Write("Введите пятизначное число:  ");
+string? number=Console.ReadLine();
+ 
+  void CheckingNumber (string number)
+ {
+    if (number[0]==number[4]|| number[1]==number[3]){
+     System.Console.WriteLine($"{number} Палидром");
+     }
+     else System.Console.WriteLine($"{number} не палидром");
+ }
+  if (number!.Length == 5){
+    CheckingNumber(number);
+  }
+  else System.Console.WriteLine("Введите правильное число");
