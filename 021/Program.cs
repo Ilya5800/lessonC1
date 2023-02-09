b611e95bda54c4b3586fa2bb63cd743cad50e543
Ilya5800 <ilyasemin870@gmail.com>
2023-02-09 12:59:06 +0300

    023

--- 021/Program.cs
+++ 021/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿//Определить номер четверти плоскости, в которой находится точка с координатами Х и У, причем X ≠ 0 и Y ≠ 0
+int x = Convert.ToInt32(Console.ReadLine());
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (x>0 & y>0)
+{
+System.Console.WriteLine("Second");
+}
+if (x<0 & y>0)
+{
+    System.Console.WriteLine("First");
+}
+if (x>0 & y<0){
+    System.Console.WriteLine("three");
+}
+ if (x<0 & y<0)
+ {
+    System.Console.WriteLine("four");
+ }
+ if (x==0 || y==0) System.Console.WriteLine("Error");
