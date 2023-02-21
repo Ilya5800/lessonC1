e50edfa7b5a08462f75e878e29190d7ad6193fd7
Ilya5800 <ilyasemin870@gmail.com>
2023-02-21 12:53:24 +0300

    Work4

---
+++ BasicAlgorithms/0230/Program.cs
@@ -0,0 +1,36 @@
+﻿//int a=10;
+//int b=15;
+/*int k=0;
+int GCD(int a, int b)
+{ 
+while(a!=b)
+{ 
+k++;
+   if (a>b)
+   a=a-b;
+   else
+   b=b-a;
+}
+return a;
+}
+   System.Console.WriteLine(GCD(15,2000));
+   System.Console.WriteLine(k);
+   */
+
+   //euqlid quike
+    int k=0;
+   int QuickGCD(int a, int b)
+   {
+     while(a!=0 && b!=0)
+     {
+        k++;
+        if (a>b)
+        a=a%b;
+        else
+        b=b%a;
+     }
+return a+b;
+
+   }
+   System.Console.WriteLine(QuickGCD(15,2000));
+   System.Console.WriteLine(k);
